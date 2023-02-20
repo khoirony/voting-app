a5f0eea81d7fbaa171c872dec8ea26be5c2177ce
“khoirony” <khoironyarief08@gmail.com>
2023-02-21 00:33:50 +0700

    episode 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,40 @@
         
         <main class="container mx-auto flex" style="max-width:1000px">
             <div class="w-70 mr-5">
-                Add idea form goes here. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam aliquam, fugit vitae in saepe laboriosam officia porro recusandae quia voluptatibus quidem? Cupiditate magnam voluptates illum maxime eaque atque. Laboriosam omnis maxime voluptatem magnam veritatis harum animi vel quaerat ea quisquam.
+                <div class="border-2 border-blue bg-white rounded-xl mt-16">
+                    <div class="text-center px-6 py-2 pt-6">
+                        <h3 class="font-semibold text-base">Add an idea</h3>
+                        <p class="text-xs mt-4">Let us know what you would like and we'll take a look over!</p>
+                    </div>
+
+                    <form action="#" method="post" class="space-y-4 px-4 py-6">
+                        <div>
+                            <input type="text" name="" id="" class="text-sm w-full border-none bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Your Idea">
+                        </div>
+                        <div>
+                            <select class="text-sm w-full rounded-xl px-4 py-2 border-none bg-gray-100" name="category" id="category">
+                                <option value="Category One">Category One</option>
+                                <option value="Category Two">Category Two</option>
+                                <option value="Category Three">Category Three</option>
+                                <option value="Category Four">Category Four</option>
+                            </select>
+                        </div>
+                        <div>
+                            <textarea name="" id="" cols="30" rows="4" class="text-sm w-full border-none bg-gray-100 rounded-xl placeholder-gray-900 px-4 py-2" placeholder="Describe your idea"></textarea>
+                        </div>
+                        <div class="flex items-center justify-between space-x-3">
+                            <button type="button" class="flex items-center justify-center w-1/2 h-11 text-xs bg-gray-200 font-semibold rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-2">
+                                <svg class="text-gray-600 w-4 transform -rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                                </svg>
+                                <span class="ml-2">Attach</span>
+                            </button>
+                            <button type="submit" class="flex items-center justify-center w-1/2 h-11 text-xs bg-blue font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-2">
+                                <span class="ml-2 text-white">Submit</span>
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
             <div class="w-175">
                 <nav class="flex items-center justify-between text-xs">
@@ -77,6 +110,7 @@
                     {{ $slot }}
                 </div>
             </div>
+            {{-- <div class="w-24"></div> --}}
         </main>
     </body>
 </html>
