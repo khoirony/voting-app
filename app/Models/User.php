05358aad075e635002fc91f3ef53d2ec6a611da0
“khoirony” <khoironyarief08@gmail.com>
2023-02-22 16:52:40 +0700

    Episode 12

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,18 @@ class User extends Authenticatable
     {
         return $this->hasMany(Idea::class);
     }
+    public function getAvatar()
+    {
+        $firstCharacter = $this->email[0];
+        $integerToUse = is_numeric($firstCharacter)
+                        ? ord(strtolower($firstCharacter)) - 21
+                        : ord(strtolower($firstCharacter)) - 96;
+
+        // $randomInteger = rand(1,36);
+        return 'https://www.gravatar.com/avatar/'
+                .md5($this->email)
+                .'?s=200'
+                .'&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
+                .$integerToUse.'.png';
+    }
 }
